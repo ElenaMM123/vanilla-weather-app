89e3e78195ebb9c5b3726022cb0485ff8bbdaa3a
ElenaMM123 <sorina.elena.mihai@gmail.com>
2022-05-27 15:51:29 +0200

    Added relevant icon from  the API

--- src/app.js
+++ src/app.js
@@ -42,13 +42,13 @@ function formatDate(now) {
 let now = new Date();
 
 function displayTemperature(response) {
-
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -56,7 +56,13 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(now);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "4d072f10c3eab49878c4b70a4b463fbb";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=${apiKey}&units=metric`;
+let apiKey = "f9fc6090ab90840dc407a7c8f1918c18";
+let city = "Stockholm";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
