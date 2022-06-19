7d3238033945e825c308d972bead3379151dcdb9
ElenaMM123 <sorina.elena.mihai@gmail.com>
2022-06-19 08:23:33 +0200

    added Weather Forecast API

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ function formatDate(now) {
   return `${day} ${hour}:${minutes}`;
 }
 let now = new Date();
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -37,7 +37,7 @@ function displayForecast() {
               <div class="col-2">
                 <div class="weather-forcast-date">${day}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  src="https://openweathermap.org/img/wn/50d@2x.png"
                   alt=""
                   width="42"
                 />
@@ -50,6 +50,12 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "f9fc6090ab90840dc407a7c8f1918c18";
+  let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -74,6 +80,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -119,4 +126,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Stockholm");
-displayForecast();
